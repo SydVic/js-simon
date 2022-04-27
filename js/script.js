bbd02bd34e50b0271dd31593fc090fe74e6f77eb
SydVic <fabrizio.bocca27@gmail.com>
2022-04-27 13:56:15 +0200

    aggiunto pseudocodice

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// visualizzare 5 numeri casuali in html
+    // funzione per creare 5 numeri random non ripetuti
+    // stamparli in html
+
+// far partire il timer in pagina
+    // dopo 30 secondi cancellare i numeri in pagina
+
+// far comparire il prompt dopo i 30 secondi
+    // chiedere all'utente di inserire i 5 numeri visualizzati
+    // confrontare i numeri inseriti con quelli generati casualmente
+    // comunicare quali e quanti numeri sono stati indovinati
